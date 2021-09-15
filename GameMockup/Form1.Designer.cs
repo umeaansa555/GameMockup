a793625453b073cc421a4624b76c2b833f1b7833
P4 <P4@DESKTOP-EDCFC3L>
2021-09-15 14:23:37 -0400

    Add project files.

---
+++ GameMockup/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace GameMockup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Transparent;
+            this.startButton.Font = new System.Drawing.Font("MS Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.Crimson;
+            this.startButton.Location = new System.Drawing.Point(160, 249);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(331, 111);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "START GAME";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.AutoSize = true;
+            this.countdownLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.countdownLabel.Location = new System.Drawing.Point(220, 119);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(183, 29);
+            this.countdownLabel.TabIndex = 1;
+            this.countdownLabel.Text = "Game Start in: 3";
+            this.countdownLabel.Click += new System.EventHandler(this.countdownLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(678, 450);
+            this.Controls.Add(this.countdownLabel);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Title Screen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label countdownLabel;
+    }
+}
+
---
+++ GameMockup/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace GameMockup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Black;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer titlepressPlayer = new SoundPlayer(Properties.Resources.jingle);
+            titlepressPlayer.Play();
+            this.BackColor = Color.White;
+            Refresh();
+            Thread.Sleep(1000);
+
+            SoundPlayer countdownPlayer = new SoundPlayer(Properties.Resources.coin);
+            countdownPlayer.Play();
+
+
+            Thread.Sleep(1000);
+            countdownPlayer.Play();
+
+            Thread.Sleep(1000);
+            countdownPlayer.Play();
+
+
+            SoundPlayer endPlayer = new SoundPlayer(Properties.Resources.end);
+            Thread.Sleep(1000);
+            endPlayer.Play();
+
+            this.BackColor = Color.Crimson;
+            Refresh();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void countdownLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
